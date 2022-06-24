070140aad202343d37b1f28863703c7b412d9410
Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-24 12:19:42 +0000

    plan.py complete

--- checks/servises/plan.py
+++ checks/servises/plan.py
@@ -1,10 +1,14 @@
 from checks.models import ControlEvent, Object
 
 
-def primary_control():
+def has_not_control():
+    return [i for i in Object.objects.all() if len(ControlEvent.objects.filter(object=i)) == 0]
+
+
+def primary_control():   
     need_control_objects = ControlEvent.objects.filter(score__range=[80, 100]).order_by('date')
     need_control_objects = set(i.object for i in need_control_objects)
-    return list(need_control_objects)[:15]
+    return (has_not_control() + list(need_control_objects))[:15]
 
 
 def repeat_control():
