33585d1f50ade729a6e807c2c9e94802e69b34e8
Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-27 14:53:26 +0000

    fix mistakes check_me

--- checks/servises/plan.py
+++ checks/servises/plan.py
@@ -6,21 +6,27 @@ def has_not_control():
 
 
 def primary_control():   
-    need_control_objects = ControlEvent.objects.filter(score__range=[80, 100]).order_by('date')
-    need_control_objects = set(i.object for i in need_control_objects)
-    return (has_not_control() + list(need_control_objects))[:15]
+    last_control = {}
+
+    for object in Object.objects.all():
+        control_events = ControlEvent.objects.filter(object=object).order_by('-date')
+        if len(control_events) > 0:
+            last_control[object] = control_events[0].date
+
+    return [i[0] for i in sorted(last_control.items(), key=lambda item: item[1])][:15]
 
 
 def repeat_control():
-    repeat_control = {}
+    last_control = {}
+
     for object in Object.objects.all():
-        try:
-            last_control = ControlEvent.objects.filter(id=object.id, score__range=[0, 79]).order_by('-date')[0]
-            repeat_control[last_control.object] = str(last_control.date)
-        except IndexError:
-            continue
-    return [i[0] for i in sorted(repeat_control.items(), key=lambda item: item[1])][:5]
+        control_events = ControlEvent.objects.filter(object=object).order_by('-date')
+        if len(control_events) > 0:
+            if control_events[0].score < 80:
+                last_control[object] = control_events[0].date
+
+    return [i[0] for i in sorted(last_control.items(), key=lambda item: item[1])][:5]
 
 
 def make_plan():
-    return primary_control() + repeat_control()
+    return set(has_not_control() + primary_control() + repeat_control())
