8eb2775d2dc4eb2f0ff6b15bf5e4da64e58a4fb1
Votchitsev <dmitry.votchitsev@gmail.com>
2022-09-01 11:01:37 +0000

    change start page view

--- checks/views/start_page.py
+++ checks/views/start_page.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import logout
 from django.urls import reverse
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
 from checks.models import ExecutiveDirector
 from checks.servises.plan import make_plan
+from check_list.checks.servises.get_files import BreachStatistics, MainReport, download_report_not_submited
 
 
 def logout_view(request):
     logout(request)
     return redirect(reverse('start_page'))
+
 
 def start_view(request):
     context = {
@@ -16,3 +19,47 @@ def start_view(request):
         'plan': make_plan(),
     }
     return render(request, context=context, template_name='checks/index.html')
+
+
+def download_main_report(request):
+    start_date = request.GET['start_date']
+    finish_date = request.GET['finish_date']
+
+    report = MainReport(start_date, finish_date)
+    response = HttpResponse(report.download_file(),
+                                content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f"attachment;filename=report.xlsx"
+    return response
+
+
+def download_brach_statistics(request):
+    start_date = request.GET['start_date']
+    finish_date = request.GET['finish_date']
+
+    report = BreachStatistics(start_date, finish_date)
+    report.download_file()
+
+    response = HttpResponse(report.download_file(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f"attachment;filename=report.xlsx"
+    return response
+
+
+def download_report_not_submited_view(request):   
+    response = HttpResponse(download_report_not_submited(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f"attachment;filename=report.xlsx"
+    return response
+
+
+def ex_director_report_view(request):
+    report = MainReport(
+        request.GET['start_date'],
+        request.GET['finish_date'],
+        request.GET['executive_director']
+    )
+    response = HttpResponse(
+        report.download_file(),
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = f"attachment;filename=report.xlsx"
+    return response
+    
